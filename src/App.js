cbaa9f55696649208cd015e5093a3c0489a41eae
NguyenNgocQuang2001 <sinhvienuetk64@gmail.com>
2022-10-28 21:50:52 +0700

    create tetrahedron

--- src/App.js
+++ src/App.js
@@ -8,10 +8,21 @@ function App() {
 		<ambientLight intensity={0.5} />
 		<spotLight position={[0, 0, 0]} angle={0.5} penumbra={1} />
 		<pointLight position={[0, 0, 0]} />
-		<OrbitControls />
+		<OrbitControls />			
 		<Line start={[-100, 0, 0]} end={[100, 0, 0]} />
 		<Line start={[0, -100, 0]} end={[0, 100, 0]} />
 		<Line start={[0, 0, -100]} end={[0, 0, 100]} />
+
+    
+		{ /**
+		 * create tetrahedron
+		 */}
+		<Line start={[1, 1, 1]} end={[2, 2, 2]} />
+		<Line start={[1, 1, 1]} end={[1, 2, 2]} />
+		<Line start={[1, 1, 1]} end={[2, 2, 1]} />
+		<Line start={[2, 2, 2]} end={[2, 2, 1]} />
+		<Line start={[2, 2, 2]} end={[1, 2, 2]} />
+		<Line start={[1, 2, 2]} end={[2, 2, 1]} />
 	</Canvas>
   );
 }
